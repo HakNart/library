f884e06bd8ce8dc3f88fea58f06577ee2c12e042
Kha Tran <khadieptran@gmail.com>
2021-09-29 05:28:07 -0400

    First setup for the script file

---
+++ script.js
@@ -0,0 +1,15 @@
+let myLibrary = [];
+
+function Book(title, author, pages, isRead) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.isRead = isRead;
+    this.info = function() {
+        return "";
+    }
+}
+
+function addBookToLibrary() {
+    myLibrary.push()
+}
